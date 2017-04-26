03e5a4713ab140412435c329ad341920151dae44
Cornel <c.o.devroomen@student.tudelft.nl>
2017-04-26 19:48:46 +0200

    Added a list with dummy values

--- app/src/main/java/com/nervousfish/nervousfish/MainActivity.java
+++ app/src/main/java/com/nervousfish/nervousfish/MainActivity.java
@@ -8,8 +8,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lv;
+    private List<String> contacts = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,24 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        lv = (ListView) findViewById(R.id.listView);
+
+        //Retrieve the contacts from the database
+        getContacts();
+
+        lv.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, contacts));
+
+    }
+
+    public void getContacts() {
+        contacts.add("Eric");
+        contacts.add("Kilian");
+        contacts.add("Joost");
+        contacts.add("Stas");
     }
 
     @Override
