518a5eddde68d8f8534eb5bdf8ae456864a81d28
Joost Verbraeken <mail@joostverbraeken.com>
2017-05-02 09:37:23 +0200

    Remove unused import

--- app/src/main/java/com/nervousfish/nervousfish/service_locator/ModuleWrapper.java
+++ app/src/main/java/com/nervousfish/nervousfish/service_locator/ModuleWrapper.java
@@ -1,7 +1,6 @@
 package com.nervousfish.nervousfish.service_locator;
 
 import com.nervousfish.nervousfish.modules.IModule;
-import com.nervousfish.nervousfish.modules.constants.IConstants;
 
 /**
  * Wraps a module and provides a package-private method to retrieve it so that only the service_locator classes can access the module.
