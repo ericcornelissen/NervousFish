ceb71975530ea73c93da07a68608a4398fd7674a
Joost Verbraeken <mail@joostverbraeken.com>
2017-05-03 20:50:42 +0200

    Improve name constant

--- app/src/main/java/com/nervousfish/nervousfish/ConstantKeywords.java
+++ app/src/main/java/com/nervousfish/nervousfish/ConstantKeywords.java
@@ -4,7 +4,7 @@ package com.nervousfish.nervousfish;
  * Contains the keywords used in the application.
  */
 public final class ConstantKeywords {
-    public static final String SERVICE_LOCATOR = "a";
+    public static final String SERVICE_LOCATOR = "Service Locator";
 
     /**
      * Prevent instantiation
