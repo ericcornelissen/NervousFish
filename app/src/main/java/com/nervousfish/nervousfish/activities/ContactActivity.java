d98ba4088cc566ba862d730a0bdf047b5a4dc54c
Cornel <c.o.devroomen@student.tudelft.nl>
2017-05-08 12:45:42 +0200

    Create ContactActivity

---
+++ app/src/main/java/com/nervousfish/nervousfish/activities/ContactActivity.java
@@ -0,0 +1,30 @@
+package com.nervousfish.nervousfish.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.nervousfish.nervousfish.R;
+
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
