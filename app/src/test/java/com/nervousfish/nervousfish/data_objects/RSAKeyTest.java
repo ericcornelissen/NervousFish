fa5aab6cf1e359848a3227f6257d0a1164ebe905
Eric Cornelissen <ericornelissen@gmail.com>
2017-05-09 16:32:55 +0200

    Add test suite for RSAKey

---
+++ app/src/test/java/com/nervousfish/nervousfish/data_objects/RSAKeyTest.java
@@ -0,0 +1,71 @@
+package com.nervousfish.nervousfish.data_objects;
+
+import com.nervousfish.nervousfish.ConstantKeywords;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+public class RSAKeyTest {
+
+    @Test
+    public void testCanBeInstantiatedWithArbitraryValues() {
+        IKey key = new RSAKey("foo", "bar");
+        assertNotNull(key);
+    }
+
+    @Test
+    public void testGetKeyReturnsNotNull() {
+        IKey key = new RSAKey("foo", "bar");
+        assertNotNull(key.getKey());
+    }
+
+    @Test
+    public void testGetKeyReturnsNonEmptyString() {
+        IKey key = new RSAKey("foo", "bar");
+        assertNotEquals("", key.getKey());
+    }
+
+    @Test
+    public void testGetTypeReturnsRSA() {
+        IKey key = new RSAKey("foo", "bar");
+        assertEquals(ConstantKeywords.RSA_KEY, key.getType());
+    }
+
+    @Test
+    public void testEqualsWorksWithNull() {
+        IKey key = new RSAKey("foo", "bar");
+        assertFalse(key.equals(null));
+    }
+
+    @Test
+    public void testEqualsWorksWithArbitraryObject() {
+        IKey key = new RSAKey("foo", "bar");
+        assertFalse(key.equals("foobar"));
+    }
+
+    @Test
+    public void testEqualsReturnsFalseForUnequalKeys() {
+        IKey keyA = new RSAKey("foo", "bar");
+        IKey keyB = new RSAKey("hello", "world");
+        assertFalse(keyA.equals(keyB));
+    }
+
+    @Test
+    public void testEqualsReturnsTrueForEqualKeys() {
+        IKey keyA = new RSAKey("foo", "bar");
+        IKey keyB = new RSAKey("foo", "bar");
+        assertTrue(keyA.equals(keyB));
+    }
+
+    @Test
+    public void testHashCodeNotNull() {
+        IKey key = new RSAKey("foo", "bar");
+        assertNotNull(key.hashCode());
+    }
+
+}
