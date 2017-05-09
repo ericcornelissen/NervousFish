c762aea7ac67d72b1da8eb0289728e47ecc02ff4
Cornel <c.o.devroomen@student.tudelft.nl>
2017-05-09 09:29:34 +0200

    Delete servicelocator example

--- app/src/main/java/com/nervousfish/nervousfish/activities/ContactActivity.java
+++ app/src/main/java/com/nervousfish/nervousfish/activities/ContactActivity.java
@@ -1,14 +1,11 @@
 package com.nervousfish.nervousfish.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.nervousfish.nervousfish.ConstantKeywords;
 import com.nervousfish.nervousfish.R;
-import com.nervousfish.nervousfish.service_locator.IServiceLocator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,9 +17,6 @@ import java.util.List;
 public final class ContactActivity extends AppCompatActivity {
 
     private final List<String> keys = new ArrayList<>();
-    @SuppressWarnings("PMD.SingularField")
-    //In the future we will use this field more than once
-    private IServiceLocator serviceLocator;
 
     /**
      * Creates the new activity, should only be called by Android
@@ -33,12 +27,6 @@ public final class ContactActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
-
-        final Intent intent = getIntent();
-        this.serviceLocator = (IServiceLocator) intent.getSerializableExtra(ConstantKeywords.SERVICE_LOCATOR);
-
-        // Example use
-        this.serviceLocator.getConstants();
 
         final ListView lv = (ListView) findViewById(R.id.listView);
 
