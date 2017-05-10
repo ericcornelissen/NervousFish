591d9d774b59bb5f0183387fda17308809d1b997
Kilian Callebaut <mrkc1998@hotmail.com>
2017-05-10 11:10:48 +0200

    Added zxing library and qrgeneratoractivity class

---
+++ app/src/main/java/com/nervousfish/nervousfish/activities/QRGeneratorActivity.java
@@ -0,0 +1,24 @@
+package com.nervousfish.nervousfish.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Generates a QR code
+ */
+
+public class QRGeneratorActivity extends Activity {
+
+    /**
+     * Creates the new activity, should only be called by Android
+     *
+     * @param savedInstanceState Don't touch this
+     */
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+}
---
+++ app/src/test/java/com/nervousfish/nervousfish/activities/QRGeneratorActivityTest.java
@@ -0,0 +1,22 @@
+package com.nervousfish.nervousfish.activities;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Kilian on 10/05/2017.
+ */
+public class QRGeneratorActivityTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+}
