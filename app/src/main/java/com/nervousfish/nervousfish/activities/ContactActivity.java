b43216761d2da645eed1c0b2cd55e179864c60fb
Cornel <c.o.devroomen@student.tudelft.nl>
2017-05-10 13:45:34 +0200

    Give back button functionality

--- app/src/main/java/com/nervousfish/nervousfish/activities/ContactActivity.java
+++ app/src/main/java/com/nervousfish/nervousfish/activities/ContactActivity.java
@@ -1,11 +1,18 @@
 package com.nervousfish.nervousfish.activities;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
+import com.nervousfish.nervousfish.ConstantKeywords;
 import com.nervousfish.nervousfish.R;
+import com.nervousfish.nervousfish.service_locator.IServiceLocator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +35,19 @@ public final class ContactActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contact);
 
         final ListView lv = (ListView) findViewById(R.id.listView);
+        final Intent intentContactActivity = getIntent();
+
+        final ImageButton button = (ImageButton) findViewById(R.id.backButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final Activity previousActivity =
+                        (Activity) intentContactActivity.getSerializableExtra(ConstantKeywords.PREVIOUS_ACTIVITY);
+                final Intent intent = new Intent(ContactActivity.this, previousActivity.getClass());
+                intent.putExtra(ConstantKeywords.SERVICE_LOCATOR,
+                        intentContactActivity.getSerializableExtra(ConstantKeywords.SERVICE_LOCATOR));
+                startActivity(intent);
+            }
+        });
 
         //Retrieve the contacts from the database
         getKeys();
