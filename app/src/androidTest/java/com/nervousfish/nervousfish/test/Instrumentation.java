b6b8ed55c8adfd88a60760a990abc24a1bcb03d4
Kilian Callebaut <mrkc1998@hotmail.com>
2017-05-12 09:59:55 +0200

    Added Multidex functionality

--- app/src/androidTest/java/com/nervousfish/nervousfish/test/Instrumentation.java
+++ app/src/androidTest/java/com/nervousfish/nervousfish/test/Instrumentation.java
@@ -1,6 +1,7 @@
 package com.nervousfish.nervousfish.test;
 
 import android.os.Bundle;
+import android.support.multidex.MultiDex;
 import android.support.test.runner.AndroidJUnitRunner;
 import android.support.test.runner.MonitoringInstrumentation;
 
@@ -13,6 +14,7 @@ public class Instrumentation extends AndroidJUnitRunner {
 
     @Override
     public void onCreate(Bundle arguments) {
+        MultiDex.install(getTargetContext());
         mInstrumentationCore.create(arguments);
         super.onCreate(arguments);
        // start();
@@ -20,10 +22,10 @@ public class Instrumentation extends AndroidJUnitRunner {
 
     @Override
     public void onStart() {
+        mInstrumentationCore.start();
+        waitForIdleSync();
         super.onStart();
 
-        waitForIdleSync();
-        mInstrumentationCore.start();
         allFinished();
     }
 
