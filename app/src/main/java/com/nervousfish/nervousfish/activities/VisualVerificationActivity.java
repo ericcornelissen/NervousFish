d57716c1c42ef776dc26227389c7c9c630faf6a2
Eric Cornelissen <ericornelissen@gmail.com>
2017-05-15 20:35:59 +0200

    Verification activity progresses to next activity

--- app/src/main/java/com/nervousfish/nervousfish/activities/VisualVerificationActivity.java
+++ app/src/main/java/com/nervousfish/nervousfish/activities/VisualVerificationActivity.java
@@ -1,10 +1,13 @@
 package com.nervousfish.nervousfish.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.nervousfish.nervousfish.ConstantKeywords;
 import com.nervousfish.nervousfish.R;
+import com.nervousfish.nervousfish.service_locator.IServiceLocator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +20,7 @@ public class VisualVerificationActivity extends Activity {
     private static final Logger LOGGER = LoggerFactory.getLogger("VisualVerificationActivity");
     private static final int SECURITY_CODE_LENGTH = 5;
 
+    private IServiceLocator serviceLocator;
     private String securityCode = "";
 
     /**
@@ -29,9 +33,20 @@ public class VisualVerificationActivity extends Activity {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.grid);
 
+        final Intent intent = this.getIntent();
+        this.serviceLocator = (IServiceLocator) intent.getSerializableExtra(ConstantKeywords.SERVICE_LOCATOR);
+
         this.initButtons();
 
         LOGGER.info("VisualVerificationActivity created");
+    }
+
+    private void nextActivity() {
+        // TODO: Progress to the correct activity
+        final Intent intent = new Intent(this, LoginActivity.class);
+        intent.putExtra(ConstantKeywords.SECURITY_CODE, this.securityCode);
+        intent.putExtra(ConstantKeywords.SERVICE_LOCATOR, this.serviceLocator);
+        this.startActivity(intent);
     }
 
     /**
@@ -48,11 +63,12 @@ public class VisualVerificationActivity extends Activity {
                 final String button = v.getContentDescription().toString();
                 LOGGER.info("button '" + button + "' clicked");
 
-                if (securityCode.length() == VisualVerificationActivity.SECURITY_CODE_LENGTH) {
+                if (securityCode.length() > VisualVerificationActivity.SECURITY_CODE_LENGTH) {
                     LOGGER.info("Security code already long enough");
-                } else if (securityCode.length() - 1 == VisualVerificationActivity.SECURITY_CODE_LENGTH) {
+                } else if (securityCode.length() + 1 == VisualVerificationActivity.SECURITY_CODE_LENGTH) {
                     securityCode += button;
                     LOGGER.info("final code is: " + securityCode);
+                    nextActivity();
                 } else {
                     securityCode += button;
                     LOGGER.info("code so far: " + securityCode);
