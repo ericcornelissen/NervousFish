5f8196caa64dd87284990eaad522129419a8566f
Eric Cornelissen <ericornelissen@gmail.com>
2017-05-15 11:50:43 +0200

    Fill grid with clickable buttons
    Filled the grid of the GridActivity with buttons that can be clicked, upon
    clicking any button a single onClickListeners is called which for now just
    logs the "id" (set via contentDescription) of that button.

--- app/src/main/java/com/nervousfish/nervousfish/activities/GridActivity.java
+++ app/src/main/java/com/nervousfish/nervousfish/activities/GridActivity.java
@@ -1,23 +1,32 @@
 package com.nervousfish.nervousfish.activities;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import com.nervousfish.nervousfish.ConstantKeywords;
 import com.nervousfish.nervousfish.R;
-import com.nervousfish.nervousfish.service_locator.IServiceLocator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
+ * Example activity to verify identity in Bluetooth connections.
  */
 public class GridActivity extends Activity {
 
     private static final Logger LOGGER = LoggerFactory.getLogger("GridActivity");
-    private IServiceLocator serviceLocator;
+    private static final View.OnClickListener onClickListener = new View.OnClickListener() {
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void onClick(final View v) {
+            String button = v.getContentDescription().toString();
+            LOGGER.info("button '" + button + "' clicked");
+        }
+
+    };
 
     /**
      * Stuff that needs to be done when the new activity being created.
@@ -29,11 +38,31 @@ public class GridActivity extends Activity {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.grid);
 
-        System.out.println("halloooo");
-        final Intent intent = this.getIntent();
-        this.serviceLocator = (IServiceLocator) intent.getSerializableExtra(ConstantKeywords.SERVICE_LOCATOR);
+        this.initButtons();
 
         LOGGER.info("GridActivity created");
+    }
+
+    /**
+     * Initialize all the buttons.
+     */
+    private void initButtons() {
+        this.findViewById(R.id.button00).setOnClickListener(onClickListener);
+        this.findViewById(R.id.button01).setOnClickListener(onClickListener);
+        this.findViewById(R.id.button02).setOnClickListener(onClickListener);
+        this.findViewById(R.id.button03).setOnClickListener(onClickListener);
+        this.findViewById(R.id.button10).setOnClickListener(onClickListener);
+        this.findViewById(R.id.button11).setOnClickListener(onClickListener);
+        this.findViewById(R.id.button12).setOnClickListener(onClickListener);
+        this.findViewById(R.id.button13).setOnClickListener(onClickListener);
+        this.findViewById(R.id.button20).setOnClickListener(onClickListener);
+        this.findViewById(R.id.button21).setOnClickListener(onClickListener);
+        this.findViewById(R.id.button22).setOnClickListener(onClickListener);
+        this.findViewById(R.id.button23).setOnClickListener(onClickListener);
+        this.findViewById(R.id.button30).setOnClickListener(onClickListener);
+        this.findViewById(R.id.button31).setOnClickListener(onClickListener);
+        this.findViewById(R.id.button32).setOnClickListener(onClickListener);
+        this.findViewById(R.id.button33).setOnClickListener(onClickListener);
     }
 
 }
