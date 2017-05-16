c380616aea6c269c327d71c66648237cbff55118
Cornel <c.o.devroomen@student.tudelft.nl>
2017-05-16 13:30:56 +0200

    Fix checkstyle errors and put back first activity to Login

--- app/src/main/java/com/nervousfish/nervousfish/activities/RhythmCreateActivity.java
+++ app/src/main/java/com/nervousfish/nervousfish/activities/RhythmCreateActivity.java
@@ -1,21 +1,18 @@
 package com.nervousfish.nervousfish.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.nervousfish.nervousfish.R;
 
 public class RhythmCreateActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rhythm_create);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         if (getSupportActionBar() != null) {
