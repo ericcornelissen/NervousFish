8c9e5cb99d20a0d8b3a8e821f3bd684457b85a73
Joost Verbraeken <mail@joostverbraeken.com>
2017-05-17 21:43:43 +0200

    Fix Checkstyle

--- app/src/main/java/com/nervousfish/nervousfish/activities/WaitForSlaveActivity.java
+++ app/src/main/java/com/nervousfish/nervousfish/activities/WaitForSlaveActivity.java
@@ -2,11 +2,9 @@ package com.nervousfish.nervousfish.activities;
 
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import com.nervousfish.nervousfish.ConstantKeywords;
 import com.nervousfish.nervousfish.R;
