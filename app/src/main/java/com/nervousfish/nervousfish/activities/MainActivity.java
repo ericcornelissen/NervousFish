6ee2db7c94f67df813d7fb93c34bb6f9f1e74c4d
Cornel <c.o.devroomen@student.tudelft.nl>
2017-05-23 10:19:30 +0200

    Create new activity for waiting to activate bluetooth

--- app/src/main/java/com/nervousfish/nervousfish/activities/MainActivity.java
+++ app/src/main/java/com/nervousfish/nervousfish/activities/MainActivity.java
@@ -110,7 +110,7 @@ public final class MainActivity extends AppCompatActivity {
     }
 
     private void startBluetoothPairing() {
-        final Intent intent = new Intent(this, BluetoothConnectionActivity.class);
+        final Intent intent = new Intent(this, BluetoothNotActiveActivity.class);
         intent.putExtra(ConstantKeywords.SERVICE_LOCATOR, serviceLocator);
         startActivity(intent);
     }
