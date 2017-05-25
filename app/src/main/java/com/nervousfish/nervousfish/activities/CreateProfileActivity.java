4d4905f2ae17f41774f07c88bdb4f999c66059d9
Cornel <c.o.devroomen@student.tudelft.nl>
2017-05-25 18:07:04 +0200

    Dont use the exact values anymore

--- app/src/main/java/com/nervousfish/nervousfish/activities/CreateProfileActivity.java
+++ app/src/main/java/com/nervousfish/nervousfish/activities/CreateProfileActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.nervousfish.nervousfish.ConstantKeywords;
 import com.nervousfish.nervousfish.R;
@@ -37,8 +38,9 @@ public final class CreateProfileActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayShowTitleEnabled(false);
         }
 
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
-        LOGGER.info("FirstUseActivity created");
+        LOGGER.info("CreateProfileActivity created");
     }
 
     /**
