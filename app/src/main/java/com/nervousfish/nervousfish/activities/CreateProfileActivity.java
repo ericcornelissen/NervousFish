ac46970584279d2276d73f20d87e79955bff1678
Cornel <c.o.devroomen@student.tudelft.nl>
2017-05-25 17:36:43 +0200

    Create profile activity

--- app/src/main/java/com/nervousfish/nervousfish/activities/CreateProfileActivity.java
+++ app/src/main/java/com/nervousfish/nervousfish/activities/CreateProfileActivity.java
@@ -1,46 +1,23 @@
 package com.nervousfish.nervousfish.activities;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.AssetManager;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.nervousfish.nervousfish.ConstantKeywords;
 import com.nervousfish.nervousfish.R;
-import com.nervousfish.nervousfish.data_objects.Contact;
-import com.nervousfish.nervousfish.data_objects.IKey;
-import com.nervousfish.nervousfish.data_objects.SimpleKey;
-import com.nervousfish.nervousfish.events.ContactReceivedEvent;
-import com.nervousfish.nervousfish.modules.database.IDatabase;
 import com.nervousfish.nervousfish.service_locator.IServiceLocator;
 
-import org.greenrobot.eventbus.Subscribe;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 /**
  * The main activity class that shows a list of all people with their public keys
  */
-public final class FirstUseActivity extends AppCompatActivity {
+public final class CreateProfileActivity extends AppCompatActivity {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger("FirstUseActivity");
+    private static final Logger LOGGER = LoggerFactory.getLogger("CreateProfileActivity");
 
     private IServiceLocator serviceLocator;
 
@@ -54,7 +31,7 @@ public final class FirstUseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         final Intent intent = getIntent();
         this.serviceLocator = (IServiceLocator) intent.getSerializableExtra(ConstantKeywords.SERVICE_LOCATOR);
-        this.setContentView(R.layout.activity_first_use);
+        this.setContentView(R.layout.activity_create_profile);
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayShowTitleEnabled(false);
@@ -65,12 +42,12 @@ public final class FirstUseActivity extends AppCompatActivity {
     }
 
     /**
-     * Gets triggered when the GetStarted button is clicked.
+     * Gets triggered when the Submit button is clicked.
      *
      * @param v - the {@link View} clicked
      */
-    public void onGetStartedClick(final View v) {
-        final Intent intent = new Intent(this, CreateProfileActivity.class);
+    public void onSubmitClick(final View v) {
+        final Intent intent = new Intent(this, LoginActivity.class);
         intent.putExtra(ConstantKeywords.SERVICE_LOCATOR, serviceLocator);
         this.startActivity(intent);
     }
