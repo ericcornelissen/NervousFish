7e6a725f0cee859ecc709b2b088688b97b43ab63
Eric Cornelissen <ericornelissen@gmail.com>
2017-05-31 08:48:11 +0200

    Add list of verification methods

--- app/src/main/java/com/nervousfish/nervousfish/activities/SelectVerificationMethodActivity.java
+++ app/src/main/java/com/nervousfish/nervousfish/activities/SelectVerificationMethodActivity.java
@@ -2,11 +2,16 @@ package com.nervousfish.nervousfish.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.nervousfish.nervousfish.R;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Activity used to select a verification method.
@@ -14,6 +19,8 @@ import org.slf4j.LoggerFactory;
 public class SelectVerificationMethodActivity extends AppCompatActivity {
 
     private static final Logger LOGGER = LoggerFactory.getLogger("SelectVerificationMethodActivity");
+
+    private final List<String> methods = new ArrayList<>();
 
     /**
      * Creates a new {@link SelectVerificationMethodActivity} activity.
@@ -24,6 +31,14 @@ public class SelectVerificationMethodActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.select_verification_method);
+
+        // Add possible verification methods
+        this.methods.add("Visual pattern");
+        this.methods.add("Tab a rhythm");
+
+        // Set verificatio methods in the ListView
+        final ListView listView = (ListView) this.findViewById(R.id.verification_method_list);
+        listView.setAdapter(new ArrayAdapter<>(this, R.layout.verification_list_entry, this.methods));
 
         LOGGER.info("Activity created");
     }
--- app/src/main/java/com/nervousfish/nervousfish/service_locator/EntryActivity.java
+++ app/src/main/java/com/nervousfish/nervousfish/service_locator/EntryActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.nervousfish.nervousfish.ConstantKeywords;
-import com.nervousfish.nervousfish.activities.LoginActivity;
 import com.nervousfish.nervousfish.activities.SelectVerificationMethodActivity;
 
 import org.slf4j.Logger;
