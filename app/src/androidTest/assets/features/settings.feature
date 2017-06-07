2a9dc90d2f2cb8e042397f1aafa208764e10001c
Cornel <c.o.devroomen@student.tudelft.nl>
2017-06-07 12:03:37 +0200

    Create extra tests

--- app/src/androidTest/assets/features/settings.feature
+++ app/src/androidTest/assets/features/settings.feature
@@ -1,7 +1,17 @@
 Feature: Settings Activity
   Tests for the Settings Activity
 
-Scenario: Clicking the back button to go back to the main activity
+  Scenario: Clicking the back button to go back to the main activity
     Given I am viewing the settings activity
     When I click the back button in the settings activity to go back to main
     Then I should go back to the main activity from settings
+
+  Scenario: Click on general settings
+    Given I am viewing the settings activity
+    When I click on the general settings item
+    Then I should go to the general settings fragment
+
+  Scenario: Click on personal information
+      Given I am viewing the settings activity
+      When I click on the personal information item
+      Then I should go to the personal information fragment
--- app/src/androidTest/java/com/nervousfish/nervousfish/test/SettingsSteps.java
+++ app/src/androidTest/java/com/nervousfish/nervousfish/test/SettingsSteps.java
@@ -1,9 +1,12 @@
 package com.nervousfish.nervousfish.test;
 
 import android.content.Intent;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.support.test.rule.ActivityTestRule;
 
 import com.nervousfish.nervousfish.*;
+import com.nervousfish.nervousfish.R;
 import com.nervousfish.nervousfish.activities.ChangeContactActivity;
 import com.nervousfish.nervousfish.activities.SettingsActivity;
 import com.nervousfish.nervousfish.service_locator.IServiceLocator;
@@ -18,16 +21,21 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.PreferenceMatchers.withKey;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.AllOf.allOf;
 
 @CucumberOptions(features = "features")
 public class SettingsSteps {
@@ -55,8 +63,23 @@ public class SettingsSteps {
         onView(withText(com.nervousfish.nervousfish.R.string.pref_header_general)).perform(click());
     }
 
+    @When("^I click on the personal information item$")
+    public void iClickOnPersonalInformationSettings() {
+        onView(withText(com.nervousfish.nervousfish.R.string.pref_header_profile)).perform(click());
+    }
+
     @Then("^I should go back to the main activity from settings$")
     public void iShouldGoBackToMainFromSettings() {
         assertTrue(mActivityRule.getActivity().isFinishing());
+    }
+
+    @Then("^I should go to the general settings fragment$")
+    public void iShouldGoToGeneralSettingsFragment() {
+        onView(withText(R.string.pref_verification_method_to_use)).check(matches(isDisplayed()));
+    }
+
+    @Then("^I should go to the personal information fragment$")
+    public void iShouldGoToPersonalSettingsFragment() {
+        onView(withText(R.string.display_name)).check(matches(isDisplayed()));
     }
 }
