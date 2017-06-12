ec4d64b0222c840f4e281ccfa59251fd4b96763a
Yxo (stas) <staspas@hotmail.com>
2017-06-12 19:14:40 +0200

    Merge branch 'develop' into NFC-data-exchange#8
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/nervousfish/nervousfish/activities/MainActivity.java
+++ app/src/main/java/com/nervousfish/nervousfish/activities/MainActivity.java
@@ -100,6 +100,12 @@ public final class MainActivity extends AppCompatActivity {
         final Object successfulBluetooth = intent.getSerializableExtra(ConstantKeywords.SUCCESSFUL_BLUETOOTH);
         this.showSuccessfulBluetoothPopup(successfulBluetooth);
 
+        if (NfcAdapter.getDefaultAdapter(this) == null) {
+            LOGGER.info("NFC not available on device, disabling button");
+            final FloatingActionButton button = (FloatingActionButton) this.findViewById(R.id.pairing_menu_nfc);
+            button.setEnabled(false);
+        }
+
         // Initialize sorter
         this.sorter = new MainActivitySorter(this);
 
