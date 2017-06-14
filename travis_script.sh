4c8a2c065c465419ffd37d48eedd15bcea9a1368
Eric Cornelissen <ericornelissen@gmail.com>
2017-06-14 22:00:41 +0200

    Update quality component, add coverage&unittest component

--- travis_script.sh
+++ travis_script.sh
@@ -2,10 +2,14 @@
 
 set -e
 
-if [ "$COMPONENT" == "quality" ]; then
-    ./gradlew checkstyle pmd findbugs lint
-elif [ "$COMPONENT" == "build" ]; then
+if [ "$COMPONENT" == "build" ]; then
     ./gradlew build
+elif [ "$COMPONENT" == "coverage" ]; then
+    ./gradlew jacocoTestReport coveralls
+elif [ "$COMPONENT" == "quality" ]; then
+    ./gradlew checkstyle pmd lint findbugs
+elif [ "$COMPONENT" == "unittest" ]; then
+    ./gradlew testDebugUnitTest testReleaseUnitTest
 else
     echo "Unknown module"
     exit 1
