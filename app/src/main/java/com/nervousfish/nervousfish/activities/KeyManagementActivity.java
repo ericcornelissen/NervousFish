d24f6daa11114d806f80afb636cd7cdb2b6d97ea
Stas Mironov <S.A.Mironov@student.tudelft.nl>
2017-06-14 20:04:25 +0200

    Fixing the static analysis warnings, which included the refactor of the onmenuclicklistener to a static inner class in key management activity

--- app/src/main/java/com/nervousfish/nervousfish/activities/KeyManagementActivity.java
+++ app/src/main/java/com/nervousfish/nervousfish/activities/KeyManagementActivity.java
@@ -7,6 +7,10 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 import com.nervousfish.nervousfish.R;
 import com.nervousfish.nervousfish.data_objects.IKey;
@@ -21,18 +25,18 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import static android.content.ClipDescription.MIMETYPE_TEXT_PLAIN;
 
 /**
  * An {@link Activity} that used for adding, changing and deleting key pairs.
  */
-public class KeyManagementActivity extends Activity {
+public final class KeyManagementActivity extends Activity {
 
     private static final Logger LOGGER = LoggerFactory.getLogger("KeyManagementActivity");
 
-    private String[] key = {};
+    private Profile myProfile;
+    private String[] key;
 
     /**
      * Creates the new activity, should only be called by Android
@@ -42,40 +46,66 @@ public class KeyManagementActivity extends Activity {
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_key_management);
+        this.setContentView(R.layout.activity_key_management);
         final IServiceLocator serviceLocator = NervousFish.getServiceLocator();
         final IDatabase database = serviceLocator.getDatabase();
         String title = "";
-        Profile myProfile = null;
         try {
-            myProfile = database.getProfiles().get(0);
-            title = myProfile.getKeyPairs().get(0).getName();
-            key = myProfile.getKeyPairs().get(0).getPublicKey().getKey().split(" ");
+            this.myProfile = database.getProfiles().get(0);
+            title = this.myProfile.getKeyPairs().get(0).getName();
+            this.key = this.myProfile.getKeyPairs().get(0).getPublicKey().getKey().split(" ");
 
         } catch (IOException e) {
             LOGGER.error("Could not get my public key from the database ", e);
         }
 
         final ArrayList<IKey> list = new ArrayList<>();
-        for (KeyPair kp : myProfile.getKeyPairs()) {
+        for (final KeyPair kp : this.myProfile.getKeyPairs()) {
             list.add(kp.getPublicKey());
         }
 
-        ListviewActivityHelper.setKeys(this, list, R.id.list_view_contact);
+        ListviewActivityHelper.setKeys(this, list, R.id.list_view_edit_keys);
+
+        final ListView lv = (ListView) this.findViewById(R.id.list_view_edit_keys);
 
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(title);
-        builder.setMessage(String.format("This is the exponent: %s \n and this the modulus: %s ", key[0], key[1]));
+        builder.setMessage(String.format("Exponent: %s %nModulus: %s ", this.key[0], this.key[1]));
 
         builder.setPositiveButton("Copy", new DialogInterface.OnClickListener() {
-            public void onClick(final DialogInterface dialog, final int id) {
-                final ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                final ClipData clip = ClipData.newPlainText(MIMETYPE_TEXT_PLAIN, Arrays.toString(key));
+            @Override
+            public void onClick(final DialogInterface dialog, final int which) {
+                final ClipboardManager clipboard = (ClipboardManager) KeyManagementActivity.this.getSystemService(Context.CLIPBOARD_SERVICE);
+                final ClipData clip = ClipData.newPlainText(MIMETYPE_TEXT_PLAIN,
+                        KeyManagementActivity.this.myProfile.getKeyPairs().get(0).getPublicKey().getKey());
                 clipboard.setPrimaryClip(clip);
             }
         });
-        builder.show();
+
+        lv.setOnItemClickListener(new KeyManagementActivity.MyOnItemClickListener(builder));
+
+        final ImageButton backButton = (ImageButton) this.findViewById(R.id.back_button_key_management);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                KeyManagementActivity.this.finish();
+            }
+        });
 
         LOGGER.info("Activity created");
+    }
+
+    private static class MyOnItemClickListener implements AdapterView.OnItemClickListener {
+        private final AlertDialog.Builder builder;
+
+        MyOnItemClickListener(final AlertDialog.Builder builder) {
+            this.builder = builder;
+        }
+
+        @Override
+        public final void onItemClick(final AdapterView<?> parent, final View view, final int position,
+                                      final long id) {
+            this.builder.show();
+        }
     }
 }
