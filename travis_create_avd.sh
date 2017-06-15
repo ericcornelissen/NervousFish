764857261bf070b68c1cfa6dd3a3eff579652cc7
Eric Cornelissen <ericornelissen@gmail.com>
2017-06-15 11:14:10 +0200

    Wait for emulator in .travis.yml

--- travis_create_avd.sh
+++ travis_create_avd.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# Creates and starts an emulator
+# Creates and starts an emulator.
+android-update-sdk --components=sys-img-armeabi-v7a-android-22 --accept-licenses='android-sdk-license-[0-9a-f]{8}'
 echo no | android create avd --force -n test -t android-22 --abi armeabi-v7a
-emulator -avd test -no-audio -no-window &
-android-wait-for-emulator
-adb shell input keyevent 82 &
+emulator -avd test -no-audio -no-skin -no-window &
+
 
 exit 0
