817a5b984bff5a7e55ab9fe0cafceaf9a8cf567a
Cornel <c.o.devroomen@student.tudelft.nl>
2017-06-15 20:48:07 +0200

    Fix error with showing and hiding

--- app/src/main/java/com/nervousfish/nervousfish/activities/MainActivitySorter.java
+++ app/src/main/java/com/nervousfish/nervousfish/activities/MainActivitySorter.java
@@ -124,8 +124,8 @@ final class MainActivitySorter {
      * Hides the message that the user has no contacts yet.
      */
     private void hideNoContactsContent() {
-        mainActivity.findViewById(R.id.view_flipper_sorter_main).setVisibility(View.GONE);
-        mainActivity.findViewById(R.id.no_users_screen).setVisibility(View.VISIBLE);
+        mainActivity.findViewById(R.id.view_flipper_sorter_main).setVisibility(View.VISIBLE);
+        mainActivity.findViewById(R.id.no_users_screen).setVisibility(View.GONE);
     }
 
     /**
