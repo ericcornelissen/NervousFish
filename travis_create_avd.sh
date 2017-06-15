e8dbaca668990256483f512bc2ac807918594811
Eric Cornelissen <ericornelissen@gmail.com>
2017-06-15 08:11:05 +0200

    :arrow_up: Android version on emulator

--- travis_create_avd.sh
+++ travis_create_avd.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Creates and starts an emulator.
-android-update-sdk --components=sys-img-armeabi-v7a-android-16 --accept-licenses='android-sdk-license-[0-9a-f]{8}'
-echo no | android create avd --force -n test -t android-16 --abi armeabi-v7a --skin QVGA
-emulator -avd test -no-audio -no-skin -netfast -no-window &
+android-update-sdk --components=sys-img-armeabi-v7a-android-22 --accept-licenses='android-sdk-license-[0-9a-f]{8}'
+echo no | android create avd --force -n test -t android-22 --abi armeabi-v7a
+emulator -avd test -no-audio -no-skin -no-window &
 
 exit 0
