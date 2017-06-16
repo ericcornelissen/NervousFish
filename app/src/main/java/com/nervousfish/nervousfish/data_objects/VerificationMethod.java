bb6d0bd851db8abb7296cccf7c0c4b4bffcd79ed
Joost Verbraeken <j.verbraeken@hotmail.com>
2017-06-16 22:29:04 +0200

    Make clear that VerificationMethod is a wrapper

--- app/src/main/java/com/nervousfish/nervousfish/data_objects/VerificationMethod.java
+++ app/src/main/java/com/nervousfish/nervousfish/data_objects/VerificationMethod.java
@@ -7,7 +7,7 @@ import java.io.ObjectInputStream;
 import java.io.Serializable;
 
 /**
- * The only purpose of this class is to be sent over Bluetooth to let the other person know how they should
+ * The only purpose of this class is to wrap the {@link VerificationMethodEnum} that is to be sent over Bluetooth to let the other person know how they should
  * pair, i.e. by one of the methods as specified in {@link VerificationMethodEnum}.
  */
 public final class VerificationMethod implements Serializable {
