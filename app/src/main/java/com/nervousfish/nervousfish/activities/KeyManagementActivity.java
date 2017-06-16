8697bf10abde247d3604f95de265fc8b590a694d
Stas Mironov <S.A.Mironov@student.tudelft.nl>
2017-06-16 11:14:32 +0200

    More useless refactoring and findbugs/pmd errors

--- app/src/main/java/com/nervousfish/nervousfish/activities/KeyManagementActivity.java
+++ app/src/main/java/com/nervousfish/nervousfish/activities/KeyManagementActivity.java
@@ -15,6 +15,7 @@ import com.nervousfish.nervousfish.R;
 import com.nervousfish.nervousfish.data_objects.IKey;
 import com.nervousfish.nervousfish.data_objects.KeyPair;
 import com.nervousfish.nervousfish.data_objects.Profile;
+import com.nervousfish.nervousfish.modules.database.DatabaseException;
 import com.nervousfish.nervousfish.modules.database.IDatabase;
 import com.nervousfish.nervousfish.service_locator.IServiceLocator;
 import com.nervousfish.nervousfish.service_locator.NervousFish;
@@ -36,8 +37,6 @@ public final class KeyManagementActivity extends Activity {
 
     private static final Logger LOGGER = LoggerFactory.getLogger("KeyManagementActivity");
 
-    private Profile myProfile;
-
     /**
      * Creates the new activity, should only be called by Android
      *
@@ -49,16 +48,19 @@ public final class KeyManagementActivity extends Activity {
         this.setContentView(R.layout.activity_key_management);
         final IServiceLocator serviceLocator = NervousFish.getServiceLocator();
         final IDatabase database = serviceLocator.getDatabase();
+        Profile myProfile = null;
         try {
-            this.myProfile = database.getProfiles().get(0);
+            myProfile = database.getProfiles().get(0);
         } catch (final IOException e) {
             LOGGER.error("Could not get my public key from the database ", e);
+            throw new DatabaseException(e);
         }
-        final String title = this.myProfile.getKeyPairs().get(0).getName();
-        final String[] key = this.myProfile.getKeyPairs().get(0).getPublicKey().getKey().split(" ");
+        final String title = myProfile.getKeyPairs().get(0).getName();
+        final String[] key = myProfile.getKeyPairs().get(0).getPublicKey().getKey().split(" ");
 
         final List<IKey> list = new ArrayList<>();
-        for (final KeyPair kp : this.myProfile.getKeyPairs()) {
+        //noinspection Convert2streamapi Because if we want to refactor this to a stream, we have to use at least API 24
+        for (final KeyPair kp : myProfile.getKeyPairs()) {
             list.add(kp.getPublicKey());
         }
 
@@ -66,7 +68,7 @@ public final class KeyManagementActivity extends Activity {
 
         final ListView lv = (ListView) this.findViewById(R.id.list_view_edit_keys);
 
-        lv.setOnItemClickListener(new KeyManagementActivity.KeyListClickListener(this, title, key, this.myProfile));
+        lv.setOnItemClickListener(new KeyManagementActivity.KeyListClickListener(this, title, key, myProfile));
 
         final ImageButton backButton = (ImageButton) this.findViewById(R.id.back_button_key_management);
         backButton.setOnClickListener(v -> this.finish());
