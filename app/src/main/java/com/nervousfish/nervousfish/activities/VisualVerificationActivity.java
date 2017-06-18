ba4d6b406245b646b3585b8c588586c54a8bb68e
Joost Verbraeken <j.verbraeken@hotmail.com>
2017-06-18 22:57:50 +0200

    Send VisualVerification data encrypted

--- app/src/main/java/com/nervousfish/nervousfish/activities/VisualVerificationActivity.java
+++ app/src/main/java/com/nervousfish/nervousfish/activities/VisualVerificationActivity.java
@@ -85,7 +85,7 @@ public final class VisualVerificationActivity extends Activity {
                     + keyPair.getPublicKey().toString());
 
             final Contact myProfileAsContact = new Contact(profile.getName(), new Ed25519Key("Ed25519 key", "73890ien"));
-            this.serviceLocator.getBluetoothHandler().send(myProfileAsContact);
+            this.serviceLocator.getBluetoothHandler().send(myProfileAsContact, this.securityCode);
         } catch (IOException e) {
             LOGGER.error("Could not send my contact to other device " + e.getMessage());
         }
