a5368056c6ee4bbad41455b12c98b85c24aca538
Joost Verbraeken <j.verbraeken@hotmail.com>
2017-06-20 12:35:31 +0200

    Added javadoc to TestSerializable

--- app/src/test/java/com/nervousfish/nervousfish/modules/pairing/TestSerializable.java
+++ app/src/test/java/com/nervousfish/nervousfish/modules/pairing/TestSerializable.java
@@ -6,15 +6,31 @@ import java.io.InvalidObjectException;
 import java.io.ObjectInputStream;
 import java.io.Serializable;
 
+/**
+ * This class is used to be serialized whenever some test wants to serialize / deserialize something and doesn't
+ * care what the class it serializes / deserializes is.
+ * The reason we don't take a class from the app itself, is because of none of those classes we can be sure they
+ * keep existing.
+ */
 final class TestSerializable implements Serializable {
 
     private static final long serialVersionUID = -4715364587956219157L;
     private final String name;
 
+    /**
+     * Constructs a new Serializable object for test purposes with a random string as name.
+     *
+     * @param name a random String
+     */
     TestSerializable(final String name) {
         this.name = name;
     }
 
+    /**
+     * Getter for the name of this class.
+     *
+     * @return The random name of this class, that was injected in the constructor
+     */
     public String getName() {
         return this.name;
     }
