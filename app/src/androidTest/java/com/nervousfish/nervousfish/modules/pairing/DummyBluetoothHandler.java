dd0c3f0d9ffa75a444e93c79860290367ea61006
Eric Cornelissen <ericornelissen@gmail.com>
2017-06-22 11:06:56 +0200

    Remove DummyBluetoothHandler from the code base

--- app/src/androidTest/java/com/nervousfish/nervousfish/modules/pairing/DummyBluetoothHandler.java
+++ app/src/androidTest/java/com/nervousfish/nervousfish/modules/pairing/DummyBluetoothHandler.java
@@ -12,25 +12,14 @@ import org.slf4j.LoggerFactory;
  * A Bluetooth handler without implementation, needed because Bluetooth is unavailable on the emulator
  */
 public final class DummyBluetoothHandler extends APairingHandler implements IBluetoothHandler {
+
     private static final Logger LOGGER = LoggerFactory.getLogger("DummyBluetoothHandler");
 
-    /**
-     * Prevents construction from outside the class.
-     *
-     * @param serviceLocator Can be used to get access to other modules
-     */
     private DummyBluetoothHandler(final IServiceLocator serviceLocator) {
         super(serviceLocator);
         LOGGER.info("Initialized");
     }
 
-    /**
-     * Creates a new instance of itself and wraps it in a {@link ModuleWrapper} so that only an
-     * {@link IServiceLocator}
-     *
-     * @param serviceLocator The servicelocator that gives access to the other modules
-     * @return A wrapper around a newly created instance of this class
-     */
     public static ModuleWrapper<DummyBluetoothHandler> newInstance(final IServiceLocator serviceLocator) {
         return new ModuleWrapper<>(new DummyBluetoothHandler(serviceLocator));
     }
@@ -54,4 +43,5 @@ public final class DummyBluetoothHandler extends APairingHandler implements IBlu
     public void send(final byte[] buffer) {
         // Do nothing
     }
+
 }
