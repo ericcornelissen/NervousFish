325909a3ebbf756574de87799832376749fbb3fc
Kilian Callebaut <mrkc1998@hotmail.com>
2017-06-23 11:11:59 +0200

    Added more tests to database

--- app/src/test/java/com/nervousfish/nervousfish/data_objects/DatabaseTest.java
+++ app/src/test/java/com/nervousfish/nervousfish/data_objects/DatabaseTest.java
@@ -32,4 +32,37 @@ public class DatabaseTest {
         new Database(new ArrayList<>(), null);
     }
 
+    @Test
+    public void testInstantiateNoContact() {
+        List<Contact> contactList = new ArrayList<>();
+        new Database(contactList, profile);
+    }
+
+    @Test
+    public void testInstantiateSingleContact() {
+        List<Contact> contactList = new ArrayList<>();
+        contactList.add(new Contact("TestPerson1", new RSAKey("This", "is", "for testing")));
+        new Database(contactList, profile);
+    }
+
+    @Test
+    public void testInstantiateMultipleContacts() {
+        List<Contact> contactList = new ArrayList<>();
+        contactList.add(new Contact("TestPerson1", new RSAKey("This", "is", "for testing")));
+        contactList.add(new Contact("TestPerson2", new RSAKey("This", "is", "also for testing")));
+
+        new Database(contactList, profile);
+    }
+
+    @Test
+    public void testGetContacts() {
+        List<Contact> contactList = new ArrayList<>();
+        contactList.add(new Contact("TestPerson1", new RSAKey("This", "is", "for testing")));
+        contactList.add(new Contact("TestPerson2", new RSAKey("This", "is", "also for testing")));
+        Database db = new Database(contactList, profile);
+
+        assertEquals(db.getContacts().get(0).getName(), "TestPerson1");
+        assertEquals(db.getContacts().get(1).getName(), "TestPerson2");
+    }
+
 }
