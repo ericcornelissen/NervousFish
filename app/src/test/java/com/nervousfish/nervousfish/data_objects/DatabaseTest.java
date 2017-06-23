1312b9b8e30a00be2110ddac0e1169b45040feb4
Kilian Callebaut <mrkc1998@hotmail.com>
2017-06-23 09:42:35 +0200

    initial commit

---
+++ app/src/test/java/com/nervousfish/nervousfish/data_objects/DatabaseTest.java
@@ -0,0 +1,28 @@
+package com.nervousfish.nervousfish.data_objects;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+
+public class DatabaseTest {
+    private Profile profile;
+
+    @Before
+    public void setup() {
+        List<KeyPair> profilePairs = new ArrayList<>();
+        profilePairs.add(new KeyPair("Testpair", mock(IKey.class), mock(IKey.class)));
+        this.profile = new Profile("Tester", profilePairs);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testInstantiateWithNullContactListNormalProfile() {
+        new Database(null, profile);
+    }
+
+}
