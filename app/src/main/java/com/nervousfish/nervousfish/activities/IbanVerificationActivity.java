25d421eeda685190a22f02bd527ffffeaa01db07
Stas Mironov <S.A.Mironov@student.tudelft.nl>
2017-06-26 14:11:03 +0200

    Fix compile errors due to not finished methods

--- app/src/main/java/com/nervousfish/nervousfish/activities/IbanVerificationActivity.java
+++ app/src/main/java/com/nervousfish/nervousfish/activities/IbanVerificationActivity.java
@@ -1,41 +1,26 @@
 package com.nervousfish.nervousfish.activities;
 
 import android.app.Activity;
-import android.app.PendingIntent;
 import android.content.Intent;
-import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
-import android.nfc.NfcAdapter;
-import android.nfc.NfcEvent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
 import com.nervousfish.nervousfish.ConstantKeywords;
 import com.nervousfish.nervousfish.R;
 import com.nervousfish.nervousfish.data_objects.Contact;
-import com.nervousfish.nervousfish.data_objects.KeyPair;
 import com.nervousfish.nervousfish.data_objects.Profile;
-import com.nervousfish.nervousfish.modules.database.IDatabase;
-import com.nervousfish.nervousfish.modules.pairing.INfcHandler;
-import com.nervousfish.nervousfish.modules.pairing.events.NewDataReceivedEvent;
 import com.nervousfish.nervousfish.service_locator.BlockchainWrapper;
 import com.nervousfish.nervousfish.service_locator.IServiceLocator;
 import com.nervousfish.nervousfish.service_locator.NervousFish;
 
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.security.InvalidKeyException;
 
 import nl.tudelft.ewi.ds.bankver.BankVer;
-
-import static android.nfc.NdefRecord.createMime;
 
 /**
  * An {@link Activity} that beams NDEF Messages to Other Devices.
@@ -90,9 +75,9 @@ public final class IbanVerificationActivity extends Activity {
      *
      * @param view The button clicked
      */
-    public void onManualVerificationClick(final View view) {
+    public void onManualVerificationClick(final View view) throws InvalidKeyException {
         LOGGER.info("Manual verification button was pressed");
-        this.bankVer.
+        this.bankVer.createManualChallenge(contact.getIban());
     }
 
     /**
@@ -102,5 +87,6 @@ public final class IbanVerificationActivity extends Activity {
      */
     public void onBunqVerificationClick(final View view) {
         LOGGER.info("Bunq verification button was pressed");
+        this.bankVer.createOnlineChallenge(contact.getIban());
     }
 }
