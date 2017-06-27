152474a46c6336e5caecdb50c14ff0f9741bab8e
Joost Verbraeken <mail@joostverbraeken.com>
2017-06-27 11:54:22 +0200

    Add Dutch translation

--- app/src/main/java/com/nervousfish/nervousfish/activities/CreateProfileActivity.java
+++ app/src/main/java/com/nervousfish/nervousfish/activities/CreateProfileActivity.java
@@ -1,5 +1,6 @@
 package com.nervousfish.nervousfish.activities;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -130,6 +131,8 @@ public final class CreateProfileActivity extends AppCompatActivity {
         final AlertDialog.Builder alert = new AlertDialog.Builder(this);
 
         final LayoutInflater inflater = this.getLayoutInflater();
+        @SuppressLint("InflateParams")
+        // 1) Suppressed because this dialog does not need a parent
         final View dialogView = inflater.inflate(R.layout.info_keys, null);
         alert.setView(dialogView);
 
